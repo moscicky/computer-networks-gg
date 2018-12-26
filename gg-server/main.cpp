f2b4f3f0d5ef488aa17687c686fe5908b9ca8573
maciejmoscicki <moscicki.maciej619@gmail.com>
2018-12-26 13:24:36 +0100

    1. Add broadcasting users

--- gg-server/main.cpp
+++ gg-server/main.cpp
@@ -19,6 +19,10 @@
 #define QUEUE_SIZE 5
 #define USERNAME_SIZE 256
 #define MSG_SIZE 256
+#define NEW_USER_CODE 200
+#define USER_LEFT_CODE 300
+#define NEW_MSG_CODE 400
+#define USERS_LIST_CODE 500
 
 struct sockaddr_in server_address;
 int server_socket_descriptor;
@@ -31,9 +35,11 @@ struct client_thread_data {
     int socket_fd;
 };
 
+
 struct msg_data {
     int fd;
-    char* msg;
+    char sender[256];
+    char msg[256];
 };
 
 void createServerSocket(short serverPort, char* serverAddress) {
@@ -90,12 +96,49 @@ msg_data* decodeMsg(char* msg) {
     sscanf(msg, "%[^;];%[^;];\n", username, msg_text);
     msg_text[strlen(msg_text)] = '\n';
     msg_data1->fd = usersFdsMap[username];
-    msg_data1->msg = msg_text;
+    strcpy(msg_data1->sender, username);
+    strcpy(msg_data1->msg, msg_text);
     return msg_data1;
 }
 
-void *handleRequest(void *t_data)
-{
+void broadcastNewUser(char *username, int fd) {
+    std::string name(username);
+    std::string new_user_msg = std::to_string(NEW_USER_CODE) + ";" + name + ";\n";
+    std::string users_list = std::to_string(USERS_LIST_CODE) + ";";
+
+    for (int i = 0; i < clientFds.size(); i++) {
+        if (clientFds.at(static_cast<unsigned long>(i)) != fd) {
+            write(clientFds.at(static_cast<unsigned long>(i)), new_user_msg.c_str(), new_user_msg.size());
+        }
+    }
+
+    for (auto& it: usersFdsMap) {
+        users_list += (it.first + ";");
+    }
+    users_list += "\n";
+
+    write(fd, users_list.c_str(), users_list.size());
+}
+
+void broadcastUserLeft(char *username) {
+    std::string name(username);
+    std::string new_user_msg = std::to_string(USER_LEFT_CODE) + ";" + name + ";\n";
+
+    for (int i = 0; i < clientFds.size(); i++) {
+        write(clientFds.at(static_cast<unsigned long>(i)), new_user_msg.c_str(), new_user_msg.size());
+    }
+}
+
+std::string prepareMsg(char *sender, char *msg) {
+    msg[strlen(msg) - 1] = '\0';
+    std::string sender_name(sender);
+    std::string msg_text(msg);
+    std::string new_msg = std::to_string(NEW_MSG_CODE) + ";" + sender_name + ";" + msg_text + ";\n";
+
+    return new_msg;
+}
+
+void *handleRequest(void *t_data) {
     pthread_detach(pthread_self());
     auto th_data = (struct client_thread_data*)t_data;
     int client_fd = th_data->socket_fd;
@@ -108,12 +151,14 @@ void *handleRequest(void *t_data)
         if (!user_registered) {
             read(client_fd, username, sizeof(username));
             username[strlen(username) - 1] = '\0';
+            broadcastNewUser(username, client_fd);
             usersFdsMap[username] = client_fd;
             user_registered = true;
         } else {
             read(client_fd, msg, sizeof(msg));
             msg_data* msg_data1 = decodeMsg(msg);
-            write(msg_data1->fd, msg_data1->msg, MSG_SIZE);
+            std::string message = prepareMsg(username, msg_data1->msg);
+            write(msg_data1->fd, message.c_str(), message.size());
         }
     }
 }
